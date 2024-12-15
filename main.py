0d8fcc74963a27e709b5af7118e25e84f81c77ec
ritik dutta <ritikduttagd@gmail.com>
2024-12-15 06:39:22 +0530

    removed parent exception

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from parser import get_total_seconds_from_transcript
 
 
 if __name__ == "__main__":
-    exception_count = 0
 
     transcript_file = 'transcript.txt'
     total_seconds = get_total_seconds_from_transcript(transcript_file)
@@ -32,27 +31,17 @@ if __name__ == "__main__":
     print("Running re-encoding of animations...")
     run_re_encode(base_dir="animations")
 
-    try:
+    # Run 'man' video generation with probabilities
+    print("Running man video creation...")
+    run_man(total_duration, transcript_path, "animations/man",
+            sip_coffee_prob, nod_prob, yes_long_prob, fill_prob)
 
-        # Run 'man' video generation with probabilities
-        print("Running man video creation...")
-        run_man(total_duration, transcript_path, "animations/man",
-                sip_coffee_prob, nod_prob, yes_long_prob, fill_prob)
+    # Run 'girl' video generation with probabilities (no sip_coffee)
+    print("Running girl video creation...")
+    run_girl(total_duration, transcript_path, "animations",
+             girl_nod_prob, girl_yes_long_prob, girl_fill_prob)
 
-        # Run 'girl' video generation with probabilities (no sip_coffee)
-        print("Running girl video creation...")
-        run_girl(total_duration, transcript_path, "animations",
-                 girl_nod_prob, girl_yes_long_prob, girl_fill_prob)
-
-    except:
-        exception_count += 1
-        print("*"*20)
-        print(f"Exceton {exception_count} skipping")
-
-
-    print(exception_count)
     # Combine the two videos
     print("Running video combination...")
     run_combine(girl_path='girl.mp4', man_path='man.mp4', output_path='combined_video.mp4')
-    print(exception_count)
 
