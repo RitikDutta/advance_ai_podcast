4a5ca9d8be304c184dcff80f360cc6d721c40eaf
ritik dutta <ritikduttagd@gmail.com>
2025-01-03 12:25:24 +0530

    added audio mod in pipeline

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from audio_mixer import Audio_mixer
 from generate_title_desc import Generate_title
 from youtube_upload import YouTubeUploader
 from parser import Time_parser
+from audio_mod import Audio_mod
+import sys
 
 # total_time = Time_parser().get_total_seconds_from_transcript() * 1000
 
@@ -17,9 +19,11 @@ sequencer.create_sequence('female')
 sequencer2 = Sequencer(2, False)
 sequencer2.create_sequence('male')
 
-Concat_vids(25).concat_vids()
+Concat_vids(200).concat_vids()
 
 Combine_vids().run_combine()
+
+Audio_mod().process_audio_files()
 
 Audio_mixer().mix_audio()
 
@@ -27,17 +31,17 @@ title, desc = Generate_title().generate_content()
 
 print(f'Uploading to Youtube ... \n\n {title} \n\n\n {desc}')
 
-# uploader = YouTubeUploader()
-# uploader.authenticate()
-# try:
-#     uploader.upload_video(
-#         file_path="output/test.mp4",  # Required
-#         title=title,            # Required
-#         description=desc,  # Required
-#         category="24",                       # 24 Entertainment"
-#         keywords="python,youtube,upload",     # Optional, default is ""
-#         privacy_status="public",             # Optional, default is "public"
-#     )
-# except Exception as e:
-#     print(f"An error occurred: {e}")
-#     sys.exit(1)
+uploader = YouTubeUploader()
+uploader.authenticate()
+try:
+    uploader.upload_video(
+        file_path="output/test.mp4",  # Required
+        title=title,            # Required
+        description=desc,  # Required
+        category="24",                       # 24 Entertainment"
+        keywords="python,youtube,upload",     # Optional, default is ""
+        privacy_status="public",             # Optional, default is "public"
+    )
+except Exception as e:
+    print(f"An error occurred: {e}")
+    sys.exit(1)
